5a199b21fb9ac2d73a27a6abefbd33b8ea78fff5
valdenir-ziger <valdenir@alunos.utfpr.edu.br>
2023-06-02 12:14:16 -0300

    acertado os testes

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
-const db_mongoose = require('./config/db_mongoose');
-const routes      = require('./routers/route');
-const mongoose    = require('mongoose');
-const express     = require('express');
-const swaggerUI   = require('swagger-ui-express');
-const path        = require('path');
-const app         = express();
-const unirest     = require("unirest");
+const db_mongoose  = require('./config/db_mongoose');
+const routes       = require('./routers/route');
+const mongoose     = require('mongoose');
+const express      = require('express');
+const swaggerUI    = require('swagger-ui-express');
+const path         = require('path');
+const app          = express();
 var   cookieParser = require('cookie-parser');
 var   session      = require('express-session');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser()); 
 app.use(session({ secret:'valdenirziger',
-                  resave: false,
+                  name: 'APIRest',
+                  resave: true,
                   saveUninitialized:true, 
-                  cookie:{secure: false, maxAge: 3600000}}));
+                  cookie:{secure: false, maxAge: 30*60*1000}}));// 30 min
 const swaggerDocument = require('./swagger.json');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/api-docs",swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use(routes);
 
@@ -34,140 +33,6 @@ app.use(
         extended: true
     })
 )
-
-//###################### Bloco destinado ao teste ###################################
-app.get('/testeLogin', async function (req, res) {
-    var resposta = await unirest.post('http://localhost:8081/api/login') 
-        //.headers({'Accept': 'application/json', 'Content-Type': 'application/json', 'withCredentials': 'true', 'Access-Control-Allow-Origin': 'true', 'Access-Control-Allow-Credentials': 'true'})
-        .send({ "login": "admin", "senha": "admin" });
-
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeCadastrarPessoa', async function (req, res) {
-    var resposta = await unirest.post('http://localhost:8081/api/cadastrarPessoa/')
-        .send({ "login": "teste", 
-                "senha": "teste", 
-                "nome" : "teste",
-                "cpf"  : "123456789",
-                "email": "teste@teste",
-                "tipo" : 2});
-
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeEditarPessoa/:id', async function (req, res) {
-    const { id } = req.params;
-    var resposta = await unirest.put('http://localhost:8081/api/editarPessoa/'+id)
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeDeletarPessoa/:id', async function (req, res) {
-    const { id } = req.params;
-    var resposta = await unirest.delete('http://localhost:8081/api/deletarPessoa/'+id)
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeCandidatos', async function (req, res) {
-    var resposta = await unirest.get('http://localhost:8081/api/candidatos')
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeListarPessoas', async function (req, res) {
-    var resposta = await unirest.get('http://localhost:8081/api/pessoas')
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testePessoaPorID/:id', async function (req, res) {
-    const { id } = req.params;
-    var resposta = await unirest.delete('http://localhost:8081/api/buscaPessoa/'+id)
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-
-app.get('/testeCadastrarProjeto', async function (req, res) {
-    var resposta = await unirest.post('http://localhost:8081/api/cadastrarProjeto/')
-        .send({ "login": "teste", 
-                "senha": "teste", 
-                "nome" : "teste",
-                "cpf"  : "123456789",
-                "email": "teste@teste",
-                "tipo" : 2});
-
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeEditarProjeto/:id', async function (req, res) {
-    const { id } = req.params;
-    var resposta = await unirest.put('http://localhost:8081/api/editarProjeto/'+id)
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeDeletarProjeto/:id', async function (req, res) {
-    const { id } = req.params;
-    var resposta = await unirest.delete('http://localhost:8081/api/deletarProjeto/'+id)
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-app.get('/testeListarProjetos', async function (req, res) {
-    var resposta = await unirest.get('http://localhost:8081/api/projetos')
-    var  mensagem = "Codigo Retornado:" + resposta.status + " Mensagem: " + resposta.statusMessage;
-    if(resposta.body != undefined){
-        mensagem = resposta.body;
-    }
-
-    console.log(mensagem);
-    res.send(mensagem);
-});
-//###################################################################################
 
 app.listen(8081, function () {
     console.log("Servidor no http://localhost:8081")
